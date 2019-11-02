48fc6c4f48c8645f681de99e797ebfe47f3ae290
barrowsk2016 <55142821+barrowsk2016@users.noreply.github.com>
2019-11-02 12:19:53 -0400

    Add files via upload

---
+++ Project Beta/viewIssues.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <title>Campus Snapshot View</title>
+        <meta name="Campus Snapshot Staff Page">
+        <meta name ="Kyle Barrows, Brayan Villier">
+        
+        <link href="css/bootstrap.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    
+    <body>
+       <div class="text-center">
+            <h1 class="title">Report</h1>
+            <p>View all current issues on campus</p>
+        </div>
+        
+        <table>
+            <tr>
+                <th>Location</th>
+                <th>Description</th>
+                <th>Status</th>
+            </tr>
+            <?php
+            $mysqli = neq mysqli("localhost", " cen4010fal19_g17", " cen4010fal19_g17")
+        </table>
+    </body> 
+</html>
